3f8516e471338f4f7d4465d08451ccb769168d64
kiranks <kiranks@gmail.com>
2024-09-23 18:18:55 +0530

    Day1 of learning Golang

--- goIntro/main.go
+++ goIntro/main.go
@@ -1,14 +1,42 @@
 package main
 
-import ("fmt"
-	"math")
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
+	var a = "Hey"
+	fmt.Print(a, "\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	//comma ok | error syntax
+	msg, _ := reader.ReadString('\n')
+	fmt.Println("Learing Golang as a ", msg)
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Rate the Golang between 1 to 5 ", input)
+	inputconvert, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("input converted to decimal", inputconvert)
+	}
+
 	fmt.Println(value(2))
+	fmt.Println(add(12, 23))
+
+	//hitesh choudhary 
+	//Golang ep-9 
 
 }
 
-func value(x float64) float64{
+func value(x float64) float64 {
 	return math.Pow(x, 100)
 }
 
